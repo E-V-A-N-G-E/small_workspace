5fe3e0a4b93bd679a8c803057907277d806c3afa
evangeline <evanchristudas@gmail.com>
2022-05-09 19:30:10 +0530

    max_abs_diff_of_|A[i]-A[j]|+|i-j|

---
+++ max_abs_diff.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int absoluteDiff(int *arr,int n){
+    int maxSum=INT32_MIN;
+    int minSum=INT32_MAX;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=arr[i]+i;
+        if(sum>maxSum){
+            maxSum=sum;
+        }
+        if(sum<minSum){
+            minSum=sum;
+        }
+    }
+    return maxSum-minSum;
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]); 
+    int maxi=absoluteDiff(arr,n);
+    cout<<maxi;
+}
