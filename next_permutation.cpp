939da837c48490272a2aeb717ca72a83c0fcdcbf
evangeline <evanchristudas@gmail.com>
2022-05-10 15:32:26 +0530

    next greater permutation

---
+++ next_permutation.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+int * nextPermute(int arr[],unsigned int n){
+    int midmax;
+    int i=n-1;
+    for(i=n-1;i>0;i--){
+        if(arr[i-1]<arr[i]){
+            midmax=arr[i];
+            arr[i]=arr[i-1];
+            arr[i-1]=midmax;
+            break;
+        }
+    }
+    if(i!=0){    
+        for(i;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[j]>arr[i]){
+                    midmax=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=midmax;
+                }
+            }
+        }
+    }
+    return arr;
+}
+int main(){
+    int arr[] = {1, 2, 10, 6, 7, 8};
+    unsigned int n = sizeof(arr)/sizeof(arr[0]);
+    int * p=nextPermute(arr, n);
+    for(int i=0;i<n;i++){
+        cout<<p[i]<<" ";
+    }
+}
