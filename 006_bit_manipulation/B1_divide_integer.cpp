ac28b782724e319dbf0db315d52564cd2613b44e
evangeline <evanchristudas@gmail.com>
2022-08-11 14:35:50 +0530

    bits

--- 006_bit_manipulation/B1_divide_integer.cpp
+++ 006_bit_manipulation/B1_divide_integer.cpp
@@ -8,12 +8,12 @@ Also, consider if there can be overflow cases. For overflow case, return INT_MAX
 using namespace std;
 
 int main(){
-    int a=5;
-    int b=2;
+    int a=9;
+    int b=3;//00011
     unsigned long long int q=0;
     unsigned long long int t=0;
     for(int i=31;i>=0;i--){
-        if((t+(b<<i))<=a){
+        if((t+(b<<i))<=a){//i=2, 
             t+=(b<<i);
             q=q|(1<<i);
         }
